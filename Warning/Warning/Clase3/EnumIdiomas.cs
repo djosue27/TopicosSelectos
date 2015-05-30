5e60ffbf8f6f712c763c34b9695e6eefa95bb003
djosue27 <djosue27@live.com>
2015-05-30 15:31:03 -0600

    Version inicial de hello world multilenguaje

---
+++ Warning/Warning/Clase3/EnumIdiomas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warning.Clase3
+{
+    public enum EnumIdiomas
+    {
+        Español = 0,
+        Ingles = 1
+    }
+}
---
+++ Warning/Warning/Clase3/MiHelloWorld.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warning.Clase3
+{
+    class MiHelloWorld
+    {
+        // idioma en que se muetra el mensaje
+        private EnumIdiomas language = EnumIdiomas.Ingles;
+
+        //Lista de usuarios que son bloqueados para saludarse personalizadamente
+        private readonly string[] BLOCKEDUSERS = {"Bryan","Li","Chope","Maduro"};
+        public readonly string[] HELLOWORLDMESSAGE = {"Hola mundo", "Hello world" };
+        //propiedad
+        public int IdiomaEnInt
+        {
+            get { return (Convert.ToInt32(language)); }
+        }
+
+        public MiHelloWorld(EnumIdiomas selectedLanguage) 
+        { 
+            
+        }
+
+        //mensajes de HelloWorld en diferentes idiomas
+        public string HelloWorld ()
+        {
+            string resultado = HELLOWORLDMESSAGE[IdiomaEnInt];
+            return resultado;
+        }
+        //overloading
+        public string HelloWorld(string userName)
+        {
+            string resultado = HelloWorld() + ", " + userName;
+            return resultado;
+        }
+        public string HelloWorldBaneado(string userName)
+        {
+            string resultado = "Hello world";
+            if (!BLOCKEDUSERS.Contains(userName))
+            {
+                resultado = HelloWorld(userName);
+            }
+            return resultado;
+        }
+    }
+}
